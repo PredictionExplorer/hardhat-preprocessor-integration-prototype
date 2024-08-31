57f786c3fab99034f1db9a31b1b57e0606bb125e
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2024-08-31 11:28:57 -0400

    Trashing, rather than deleting gas report.

--- test-2.bash
+++ test-2.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 clear
+
+# Issue. This will not necessarily work on any Linux distro, let alone on MacOS.
+# todo-3 To be fixed.
 gio trash --force gas-report.txt
+
 REPORT_GAS=true npx hardhat test
 cat gas-report.txt
